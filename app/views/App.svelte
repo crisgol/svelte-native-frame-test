a5018c5589b90d5ef31964d55849a63cd88c8515
crisgol <cristano@golsistemas.com.br>
2019-10-20 11:36:09 -0400

    test frame

---
+++ app/views/App.svelte
@@ -0,0 +1,36 @@
+<page>
+    <actionBar title="Test FRAME" icon="" flat="true">
+        
+        
+    </actionBar>
+    <gridLayout rows="300,*" columns="*">
+        <gridLayout row="0" rows="50, 70, auto, auto,auto,auto,auto" columns="200, * ">
+
+         
+            <button text="Frame" on:tap="{showFrame}" col="0" row="1" />
+
+        </gridLayout>
+
+       
+    </gridLayout>
+
+
+   
+</page>
+
+<script>
+    import {showModal, navigate,navigateTo, navigateBack } from "svelte-native"
+   
+
+    import DetailPage from "./DetailPage.svelte"
+    
+  
+
+    function showFrame() {
+        navigate({
+            page: DetailPage,
+            frame:"detail-page-frame"
+        })
+    }
+    
+</script>
---
+++ app/views/DetailPage.svelte
@@ -0,0 +1,9 @@
+<frame id="detail-page-frame">
+	<page>
+        <label text="Detail Page" />
+        <button text="Close me" on:tap="{ () => closeModal('hi from modal') }" />
+	</page>
+</frame>
+<script>
+        import { closeModal } from 'svelte-native'
+</script>
